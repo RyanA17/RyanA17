01547f9505936eec17a2ba75d87a04ec92c61fef
RyanA17 <ryan.achong@unh.edu>
2024-06-26 16:23:58 -0400

    Made a main function

---
+++ CNC Drawing.c
@@ -0,0 +1,26 @@
+//
+//  CNC Drawing.c
+//
+//
+//  Created by Ryan A on 6/26/24.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+#define ROW 50 /* max rows */
+#define COL 50 /* max columns */
+
+/*-------------------------
+Functions
+-------------------------*/
+void circle;
+
+
+int main(void){
+
+int matrix[i][j];
+
+
+return 0;
+}
